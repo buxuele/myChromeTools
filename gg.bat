2b17afb5a8dd682b3dec53c823f0ef817e2d6137
buxuele <baogebuxuele@163.com>
2025-06-11 18:34:20 +0800

    add yml

--- gg.bat
+++ gg.bat
@@ -5,11 +5,12 @@ git branch -M main
 :: 显示当前的 Git 状态
 git status
 
+:: 将当前目录下的所有更改添加到 Git 的暂存区
+git add .
+
 :: 提示用户输入提交信息
 set /p message="Enter commit message: "
 
-:: 将当前目录下的所有更改添加到 Git 的暂存区
-git add .
 
 :: 使用用户输入的提交信息来提交暂存区的更改
 git commit -m "%message%"
